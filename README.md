7a76604c8f591eb64ca5de22cec618f15d134c58
7le <silk.heqian@gmail.com>
2018-11-13 00:11:08 +0800

    添加README.md 🏹

---
+++ README.md
@@ -0,0 +1,3 @@
+# shine-mq-demo 
+
+> shine-mq SDK对应的demo
---
+++ distributed-transaction/src/main/java/top/arkstack/shine/mq/demo/demo/DistributedTranApplication.java
@@ -0,0 +1,12 @@
+package top.arkstack.shine.mq.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTranApplication.class, args);
+    }
+}
