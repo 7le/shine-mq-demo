0a30f9450195adc889d2fddc0dc99cd2ac554d00
7le <silk.heqian@gmail.com>
2019-01-13 00:20:04 +0800

    增加私信队列demo 🎈

--- distributed-transaction-consumer/src/main/java/top/arkstack/shine/mq/demo/Consumer.java
+++ distributed-transaction-consumer/src/main/java/top/arkstack/shine/mq/demo/Consumer.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import top.arkstack.shine.mq.RabbitmqFactory;
 import top.arkstack.shine.mq.bean.SendTypeEnum;
+import top.arkstack.shine.mq.constant.MqConstant;
 import top.arkstack.shine.mq.processor.BaseProcessor;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 @Component
 public class Consumer {
@@ -19,10 +21,15 @@ public class Consumer {
     @PostConstruct
     public void test() {
         //服务B 配置消费者
-        factory.add("distributed_transaction_exchange", "distributed_transaction_exchange",
+        factory.addDLX("distributed_transaction_exchange", "distributed_transaction_exchange",
                 "distributed_transaction_routekey", new ProcessorTest(), SendTypeEnum.DISTRIBUTED);
-        factory.add("dis_test", "dis_test",
+
+        factory.addDLX("dis_test", "dis_test",
                 "dis_test_key", new ProcessorTest2(), SendTypeEnum.DISTRIBUTED);
+
+        //配置死信队列 失败时候处理
+        factory.add(MqConstant.DEAD_LETTER_QUEUE, MqConstant.DEAD_LETTER_EXCHANGE,
+                MqConstant.DEAD_LETTER_ROUTEKEY, new ProcessorException(),SendTypeEnum.DLX);
     }
 
     /**
@@ -34,6 +41,7 @@ public class Consumer {
         public Object process(Object msg, Message message, Channel channel) {
             //执行服务B的任务
             System.out.println("No1 distributed transaction process: " + msg);
+            int a = 1 / 0;
             //分布式事务消息默认自动回执
             return null;
         }
@@ -49,6 +57,19 @@ public class Consumer {
             //执行服务B的任务
             System.out.println("No2 distributed transaction process: " + msg);
             //分布式事务消息默认自动回执
+            return null;
+        }
+    }
+
+    /**
+     * 处理异常
+     */
+    static class ProcessorException extends BaseProcessor {
+
+        @Override
+        public Object process(Object msg, Message message, Channel channel) {
+            //执行失败的任务，可以自行实现 通知人工处理 或者回调原服务A的回滚接口
+            System.out.println("自行实现 通知人工处理 或者回调原服务A的回滚接口：" + msg);
             return null;
         }
     }
